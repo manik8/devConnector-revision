6e6dabc65d9b5845204635d8eb5c0b6c09207cd7
manik8 <maniksharma2014.ms@gmail.com>
2020-08-10 01:12:41 +0530

    Register and login api error

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,100 @@
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = {
+        email,
+        password,
+      };
+      console.log(user);
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const body = JSON.stringify(user);
+
+      console.log(body);
+
+      const res = await axios.post("/api/auth", body, config);
+
+      console.log(res.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+
+    //   try {
+    //     const newUser = {
+    //       name,
+    //       email,
+    //       password,
+    //     };
+
+    //     const config = {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     };
+    //     const body = JSON.stringify(newUser);
+
+    //     const res = await axios.post("/api/users", body, config);
+    //     console.log(res.data);
+    //   } catch (err) {
+    //     console.error(err.response.data);
+    //   }
+  };
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"> Sign In your Account</i>
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+export default Login;
